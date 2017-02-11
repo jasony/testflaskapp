ebbcbbac4283c5b687c449b8b7882e630e9888cf
Jason <jyuen.nyc@gmail.com>
2017-02-11 02:23:41 -0500

    Add Procfile try8

--- src/run.py
+++ src/run.py
@@ -1,14 +1,12 @@
-#from src.app import app
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello World!"
-		
-__author__ = 'jslvtr'
+@app.route("/")
+def hello():
+    return "Hello world!"
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(hfost='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
